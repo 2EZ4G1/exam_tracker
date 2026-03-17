1a808d958a2e1ce80955f9403511e1f1f2abf2cf
Jiwan Singh <jiwansingh3004@gmail.com>
2026-03-17 13:15:08 +0530

    changed debug value for render and added external hostname

--- ExamTracker/settings.py
+++ ExamTracker/settings.py
@@ -26,9 +26,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-zq!sc6sx(&4+rp$+5y#f3xz#^zel$cjr5!-t=k@oc^i-7_zv*f')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
